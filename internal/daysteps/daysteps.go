839c11618b014686219602525ceb55bf2d586454
Kost <you@example.com>
2025-03-18 10:23:50 +0000

    Отладил функцию "parsePackage" в пакете "daysteps"

--- internal/daysteps/daysteps.go
+++ internal/daysteps/daysteps.go
@@ -1,6 +1,10 @@
 package daysteps
 
 import (
+	"errors"
+	"fmt"
+	"strconv"
+	"strings"
 	"time"
 )
 
@@ -10,14 +14,34 @@ var (
 
 func parsePackage(data string) (int, time.Duration, error) {
 	// ваш код ниже
+	var s []string = strings.Split(data, ",")
+
+	if len(s) != 2 {
+		return 0, time.Duration(0), errors.New("data != 2")
+	}
+
+	counStep, err := strconv.Atoi(s[0])
+	if err != nil {
+		fmt.Println(s[0])
+		return 0, time.Duration(0), err
+	}
+
+	tDuration, err := time.ParseDuration(s[1])
+	if err != nil {
+		fmt.Println(s[1])
+		return 0, time.Duration(0), err
+	}
+
+	return counStep, tDuration, nil
+
 }
 
-// DayActionInfo обрабатывает входящий пакет, который передаётся в
-// виде строки в параметре data. Параметр storage содержит пакеты за текущий день.
-// Если время пакета относится к новым суткам, storage предварительно
-// очищается.
-// Если пакет валидный, он добавляется в слайс storage, который возвращает
-// функция. Если пакет невалидный, storage возвращается без изменений.
+// DayActionInfo обрабатывает входящий пакет, который передаётся в виде строки в параметре data. Параметр storage содержит пакеты за текущий день.
+// Если время пакета относится к новым суткам, storage предварительно очищается.
+// Если пакет валидный, он добавляется в слайс storage, который возвращает функция. Если пакет невалидный, storage возвращается без изменений.
+
 func DayActionInfo(data string, weight, height float64) string {
 	// ваш код ниже
+	// fmt.Println(parsePackage(data))
+	return fmt.Sprint(parsePackage(data))
 }
